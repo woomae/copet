de451c9183dc33a76d4f562895bc0943704a0eb5
woomae <cwg0809@naver.com>
2024-01-16 00:52:59 +0900

    Update auth.service.ts

--- server/src/apis/auth/auth.service.ts
+++ server/src/apis/auth/auth.service.ts
@@ -28,7 +28,6 @@ export class AuthService {
         .cookie('user', usercookie, {
           sameSite: 'lax',
           path: '/',
-          httpOnly: true,
           expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000), // 만료기간 5일
           domain: rooturl,
         })
