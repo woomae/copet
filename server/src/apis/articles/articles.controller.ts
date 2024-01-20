a39275cca03aad12085465acc0021057869060e8
woomae <cwg0809@naver.com>
2024-01-20 19:23:17 +0900

    엔티티 수정 및 로그인 쿠키 수정
    - 엔티티 수정 및 로그인 쿠키 수정

--- server/src/apis/articles/articles.controller.ts
+++ server/src/apis/articles/articles.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
-import { Article } from './article.entity';
+import { Articles } from './articles.entity';
 
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Get('/:id')
-  getArticleById(@Param('id') id: number): Promise<Article> {
+  getArticleById(@Param('id') id: number): Promise<Articles> {
     return this.articlesService.getArticleById(id);
   }
 }
--- server/src/apis/users/users.entity.ts
+++ server/src/apis/users/users.entity.ts
@@ -1,13 +1,52 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Articles } from '../articles/articles.entity';
 
-@Entity() // 이 부분이 추가되어야 함
+@Entity()
 export class Users extends BaseEntity {
   @PrimaryGeneratedColumn()
   _id: number;
 
-  @Column()
+  @Column({ unique: true })
   email: string;
+
+  @Column({ nullable: true })
+  catagory: string;
+
+  @Column({ nullable: true })
+  region: string;
 
   @Column()
   name: string;
+
+  @Column({ unique: true, nullable: true })
+  nickname: string;
+
+  @Column({ nullable: true })
+  petimg: string;
+
+  @Column({ nullable: true })
+  petkeyword: string;
+
+  @OneToOne(() => Articles, (article) => article.owner)
+  @JoinColumn({ name: 'owner_id' })
+  article: Articles;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+  })
+  updated_at: Date;
 }
