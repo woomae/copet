2e97d44bb3779b3ef80b96313eb8181bc5622595
woomae <cwg0809@naver.com>
2024-03-02 17:15:51 +0900

    multer
    - multer

--- server/src/apis/users/users.controller.ts
+++ server/src/apis/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Users } from './users.entity';
 import { UsersService } from './users.service';
 import { StandardResponseDto } from 'src/dto/standard-response.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('users')
 export class UsersController {
@@ -11,11 +21,24 @@ export class UsersController {
     return this.usersService.findAll(); //수정 필요 보안적으로 위험 및 필요없음(테스트용)
   }
   @Patch(':id/init')
+  @UseInterceptors(
+    //사진저장 미들웨어
+    FileInterceptor('petimg', {
+      storage: diskStorage({
+        destination: './uploads', // 파일 저장 경로를 지정합니다.
+        filename: (req, file, callback) => {
+          const filename = `${Date.now()}-${file.originalname}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
   async initUser(
+    @UploadedFile() file: Express.Multer.File,
     @Param('id') id: number,
     @Body() updatedUser: Partial<Users>,
   ): Promise<StandardResponseDto> {
-    const result = await this.usersService.initUser(id, updatedUser);
+    const result = await this.usersService.initUser(id, updatedUser, file);
     if (!result)
       return new StandardResponseDto(400, 'bad request', 'can not init user');
     return new StandardResponseDto(201, 'api.common.created', result);
--- server/src/apis/users/users.service.ts
+++ server/src/apis/users/users.service.ts
@@ -31,12 +31,19 @@ export class UsersService {
   async createUser(user: Users): Promise<Users> {
     return await this.usersRepository.save(user);
   }
-  async initUser(id: number, updatedUser: Partial<Users>): Promise<Users> {
+  async initUser(
+    id: number,
+    updatedUser: Partial<Users>,
+    file: Express.Multer.File,
+  ): Promise<Users> {
+    // 파일 경로를 추출하여 업데이트할 유저 정보에 추가
+    const filePath = file.path; // 파일 경로 추출
     //객체의 모든 값이 null or undefined or 빈문자열이 아닌지 확인
     if (Object.values(updatedUser).some((value) => !value))
       throw new BadRequestException('Invalid input userdata');
+    const newUpdateUser = { ...updatedUser, petimg: filePath };
     //update
-    await this.usersRepository.initUser({ _id: id }, updatedUser);
+    await this.usersRepository.initUser({ _id: id }, newUpdateUser);
     return await this.usersRepository.findUser({ where: { _id: id } });
   }
 }
