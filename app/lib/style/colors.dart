710aeeed82be39ce5203ee3f2b79834748f9a523
강태양 <zxcv2987@naver.com>
2024-03-03 22:44:44 +0900

    color, icon Theme 추가

--- app/lib/style/colors.dart
+++ app/lib/style/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color PRIMARY_COLOR = Color(0xffF67F00);
 const Color WHITE = Color(0xffffffff);
+const Color BLACK = Color(0xff000000);
--- app/lib/style/icons.dart
+++ app/lib/style/icons.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Icon GO_BACK_ICON = Icon(
-    Icons.arrow_back_ios_new);
+    Icons.arrow_back_ios);
 
+const Icon NENT_ICON = Icon(
+  Icons.arrow_forward_ios);
