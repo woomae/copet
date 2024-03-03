20b08d6f03a2e95a0129d514578eb922973c1632
강태양 <zxcv2987@naver.com>
2024-03-03 22:43:14 +0900

    textTheme 추가, Flutter 특유의 버튼 애니메이션 제거

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,7 +6,9 @@ import 'package:pet/login/login_type.dart';
 import 'package:auth_buttons/auth_buttons.dart';
 import 'package:pet/login/login_end.dart';
 import 'package:pet/common/component/appbar.dart';
-
+import 'package:pet/pages/community/community_main.dart';
+import 'package:pet/pages/community/posting.dart';
+import 'package:pet/pages/profile/setting/setting_home.dart';
 
 void main() {
   runApp(
@@ -22,7 +24,19 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: logintype(),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          bodySmall : TextStyle(fontSize: 12.0),
+          bodyMedium : TextStyle(fontSize: 15.0),
+          bodyLarge: TextStyle(fontSize: 20.0)
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            overlayColor: MaterialStateProperty.resolveWith((states) => Colors.transparent)
+          )
+        )
+      ),
+      home: Community(),
     );
   }
 }
