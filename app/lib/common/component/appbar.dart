338ba9e14cf194a40949fb8b9769ec008df90b97
강태양 <zxcv2987@naver.com>
2024-03-03 22:45:23 +0900

    appbar 모듈화

--- app/lib/common/component/appbar.dart
+++ app/lib/common/component/appbar.dart
@@ -1,27 +1,47 @@
 import 'package:flutter/material.dart';
 
-class Appbar extends StatelessWidget {
-  const Appbar({super.key});
+// class Appbar extends StatelessWidget {
+//   const Appbar({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         //titleSpacing: 0,
+//         backgroundColor: Colors.white,
+//         elevation: 0,
+//         title: Text(
+//           'COPET',
+//           style: TextStyle(
+//             fontFamily: 'Poetsen',
+//             color: Colors.black,
+//             fontSize: 25,
+//           ),
+//         ),
+//         centerTitle: false,
+//       ),
+//     );
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        //titleSpacing: 0,
+PreferredSizeWidget Appbar() {
+  return AppBar(
+    titleSpacing: 0,
         backgroundColor: Colors.white,
         elevation: 0,
-        title: Text(
-          'COPET',
-          style: TextStyle(
-            fontFamily: 'Poetsen',
-            color: Colors.black,
-            fontSize: 25,
+        title: const Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            'COPET',
+            style: TextStyle(
+              fontFamily: 'Poetsen',
+              color: Colors.black,
+              fontSize: 25,
+            ),
           ),
         ),
         centerTitle: false,
-      ),
-    );
-  }
+  );
 }
