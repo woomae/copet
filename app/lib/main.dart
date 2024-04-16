cd0988584cc9be1ef2f66cdabf13f522a95a75f3
강태양 <zxcv2987@naver.com>
2024-04-16 21:26:47 +0900

    profilePage userData 부분 Provider 활용

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,33 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pet/login/login_area.dart';
-import 'package:pet/login/login_main.dart';
-import 'package:pet/login/login_name.dart';
-import 'package:pet/login/login_type.dart';
-import 'package:auth_buttons/auth_buttons.dart';
-import 'package:pet/login/login_end.dart';
-import 'package:pet/common/component/appbars/appbar.dart';
+import 'package:pet/api/getUser.dart';
 import 'package:pet/main/main_home.dart';
-import 'package:pet/pages/community/community_main.dart';
-import 'package:pet/pages/profile/profile_main.dart';
-import 'package:pet/pages/profile/setting/setting_home.dart';
+import 'package:pet/providers/user_notifier_provider.dart';
 import 'package:pet/style/colors.dart';
 
 void main() async{
   await dotenv.load(fileName: ".env");
+  
   runApp(
-      ProviderScope(
+      const ProviderScope(
           child:
           _App()),
   );
 }
-class _App extends StatelessWidget {
-
+class _App extends ConsumerWidget {
   const _App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.invalidate(UserProvider);
+
+    final userData = GetUser.getUser('1')
+        .then((e) => ref.watch(UserProvider.notifier).storeUserData(e));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
