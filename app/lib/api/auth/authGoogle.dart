fca7d601300d39bd6e6e3a6eb008e0cb83ba1b4a
강태양 <zxcv2987@naver.com>
2024-04-22 16:32:18 +0900

    kakao,goolge login 로직 분리

---
+++ app/lib/api/auth/authGoogle.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AuthGoogle{
+  static Future<Response> authGoogle() async{
+    await dotenv.load(fileName: ".env");
+    String? apiKey = dotenv.env['API_KEY'];
+    final res = await Dio().get('$apiKey/auth/google');
+    print(res);
+    return res;
+  }
+}
---
+++ app/lib/api/auth/authKakao.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AuthKakao {
+  Future<Response> authKakao() async {
+    await dotenv.load(fileName: ".env");
+    String? apiKey = dotenv.env['API_KEY'];
+
+    final res = await Dio().get('$apiKey/auth/kakao');
+    return res;
+  }
+}
