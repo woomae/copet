1bcd6f26b3ffc9dc08557a866f1ca6ef1c2e96e8
강태양 <zxcv2987@naver.com>
2024-04-30 16:00:16 +0900

    Color Stlye 추가

--- app/lib/style/colors.dart
+++ app/lib/style/colors.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 const Color PRIMARY_COLOR = Color(0xffF67F00);
+const Color CATEGORY_COLOR = Color(0xffFFD159);
+
 const Color WHITE = Color(0xffffffff);
 const Color BLACK = Color(0xff000000);
-const Color GREY1 = Colors.grey;
+const Color GREY1 = Color(0xffF0F0F0);
 const Color GREY2 = Color(0xffE2E2E2);
+const Color GREY3 = Color(0xffD9D9D9);
+const Color BOLD_GREY = Color(0xff575757);
+
+const Color FONT_GREY = Color(0xffB6B6B6);
 
 const Color BACKGROUND = Color(0xffE6E6E6);
 const Color GREY_BORDER = Colors.grey;
