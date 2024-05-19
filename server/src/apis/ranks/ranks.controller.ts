5de47f49370bd415b74ba4086652b8a44ca56270
woomae <cwg0809@naver.com>
2024-05-19 18:30:58 +0900

    redis를 통한 검색어 관련 기능 추가
    - 인기 검색어 기능 추가
    - 최근 검색어 기능 추가

---
+++ server/src/apis/ranks/ranks.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
+import { RanksService } from './ranks.service';
+import { LoggingInterceptor } from 'src/common/logger/logger.intrecepter';
+import { StandardResponseDto } from 'src/dto/standard-response.dto';
+import ResponseFormat from 'src/common/response-format';
+
+@Controller('ranks')
+export class RanksController {
+  constructor(private readonly ranksService: RanksService) {}
+
+  @Post('add')
+  @UseInterceptors(LoggingInterceptor)
+  async addSearchTerm(
+    @Body('userId') userId: number,
+    @Body('term') term: string,
+  ): Promise<StandardResponseDto> {
+    let result;
+    let response;
+    try {
+      result = await this.ranksService.addSearchTerm(userId, term);
+      response = ResponseFormat.ok(result).format();
+    } catch (error) {
+      response = ResponseFormat.fail(error).format();
+    }
+    return response;
+  }
+
+  @Get('recent')
+  @UseInterceptors(LoggingInterceptor)
+  async getRecentSearchTerms(
+    @Query('userId') userId: number,
+  ): Promise<StandardResponseDto> {
+    let result;
+    let response;
+    try {
+      result = await this.ranksService.getRecentSearchTerms(userId);
+      response = ResponseFormat.ok(result).format();
+    } catch (error) {
+      response = ResponseFormat.fail(error).format();
+    }
+    return response;
+  }
+
+  @Get('top')
+  @UseInterceptors(LoggingInterceptor)
+  async getTopSearchTerms(): Promise<StandardResponseDto> {
+    let result;
+    let response;
+    try {
+      result = await this.ranksService.getTopSearchTerms();
+      response = ResponseFormat.ok(result).format();
+    } catch (error) {
+      response = ResponseFormat.fail(error).format();
+    }
+    return response;
+  }
+}
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,7 @@ import { ArticlesModule } from './apis/articles/articles.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeOrmConfig } from './configs/typeorm.config';
 import { AuthModule } from './apis/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { validate } from './configs/env-validation';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,7 +13,8 @@ import { StarsModule } from './apis/stars/stars.module';
 import { CommentsModule } from './apis/comments/comments.module';
 import { LoggingInterceptor } from './common/logger/logger.intrecepter';
 import { MocksModule } from './apis/mocks/mocks.module';
-
+import { RanksModule } from './apis/ranks/ranks.module';
+import { RedisModule, RedisModuleOptions } from '@nestjs-modules/ioredis';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -24,6 +25,19 @@ import { MocksModule } from './apis/mocks/mocks.module';
     TypeOrmModule.forRootAsync({
       useFactory: async () => await getTypeOrmConfig(),
     }),
+    RedisModule.forRootAsync({
+      inject: [ConfigService], // ConfigService 주입
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<RedisModuleOptions> => ({
+        options: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+          password: configService.get('REDIS_PASSWORD'),
+        },
+        type: 'single',
+      }),
+    }),
     AuthModule,
     ArticlesModule,
     UsersModule,
@@ -31,6 +45,7 @@ import { MocksModule } from './apis/mocks/mocks.module';
     StarsModule,
     CommentsModule,
     MocksModule,
+    RanksModule,
   ],
   controllers: [AppController],
   providers: [AppService, LoggingInterceptor],
