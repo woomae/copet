e3993b44825cc6c5cecc7026a0925d6b83cd967a
ryujebin <wpqls156@gmail.com>
2024-10-25 22:26:46 +0900

    final_merge
    final_merge

--- app/lib/pages/profile/setting/setting_home.dart
+++ app/lib/pages/profile/setting/setting_home.dart
@@ -5,62 +5,62 @@ import 'package:pet/pages/profile/setting/alarm.dart';
 import 'package:pet/style/colors.dart';
 
 class SettingHome extends StatelessWidget {
-  const SettingHome ({super.key});
+  const SettingHome({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: GoBackAppBar(),
-      body: const Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          //SettingButton(buttonName: '프로필 수정'),
-          //SettingButton(buttonName: '키워드 수정'),
-          SettingButton(buttonName: '알림 설정'),
-          SettingButton(buttonName: '계정 탈퇴'),
-        ],
-      )
-    );
+        backgroundColor: Colors.white,
+        appBar: GoBackAppBar(),
+        body: const Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            //SettingButton(buttonName: '프로필 수정'),
+            //SettingButton(buttonName: '키워드 수정'),
+            SettingButton(buttonName: '알림 설정'),
+            SettingButton(buttonName: '계정 탈퇴'),
+          ],
+        ));
   }
 }
 
 class SettingButton extends StatelessWidget {
-  const SettingButton({
-    super.key,
-    required this.buttonName
-  });
+  const SettingButton({super.key, required this.buttonName});
 
   final String buttonName;
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      decoration: const BoxDecoration(
-        color: WHITE,
-        border: Border(
-          bottom: BorderSide(width: 0.5, color: Colors.black)
-        )
-      ),
-      width: double.infinity,
-      child: TextButton(
+    return Container(
+        decoration: const BoxDecoration(
+            color: WHITE,
+            border:
+                Border(bottom: BorderSide(width: 0.5, color: Colors.black))),
+        width: double.infinity,
+        child: TextButton(
             style: TextButton.styleFrom(
               padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero
-              ),
+              shape:
+                  const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
               foregroundColor: Colors.grey,
-              ),
-              onPressed: (){
-              if(buttonName == '알림 설정'){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> alarm()));
+            ),
+            onPressed: () {
+              if (buttonName == '알림 설정') {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => alarm()));
               }
-              },
-              child: Row(
-                children: [
-                  Text(buttonName, style: const TextStyle(color: Colors.black),),
-                  const Spacer(),
-                  const Icon(Icons.arrow_forward_ios, color: Colors.black, size: 20,)
-                ],
-              ))
-    );
+            },
+            child: Row(
+              children: [
+                Text(
+                  buttonName,
+                  style: const TextStyle(color: Colors.black),
+                ),
+                const Spacer(),
+                const Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.black,
+                  size: 20,
+                )
+              ],
+            )));
   }
 }
